d4a5e75dabec2fe4550fd756b645b7d787451fac
Tra Da <minhtuantip@gmail.com>
2018-07-27 23:31:43 +0700

    add load issue information

--- src/garage/assistant/ui/main/MainController.java
+++ src/garage/assistant/ui/main/MainController.java
@@ -7,10 +7,13 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -19,6 +22,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
@@ -53,6 +57,10 @@ public class MainController implements Initializable {
     private Text memberMobile;
     
     DatabaseHandler dbHandler;
+    @FXML
+    private ListView<String> lsvIssueData;
+    @FXML
+    private JFXTextField motorID;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -241,7 +249,48 @@ public class MainController implements Initializable {
             altCnc.setContentText("Issue Operation cancelled!");
             altCnc.showAndWait();
         }
+    }
+
+    @FXML
+    private void loadBookInfo2(ActionEvent event) {
+        ObservableList<String> issueData = FXCollections.observableArrayList();
         
+        String id = motorID.getText();
+        String qr = "SELECT * FROM ISSUE WHERE id_motorbike = '" + id + "'";
+        ResultSet rs = dbHandler.excQuery(qr);
+        try {
+            while(rs.next()) {
+                String mtbID = id;
+                String mmbID = rs.getString("id_member");
+                Timestamp issTime = rs.getTimestamp("issueTime");
+                int rnwCount = rs.getInt("renew_count");
+                
+                issueData.add("Issue time: " + issTime.toGMTString());
+                issueData.add("Renew count: " + rnwCount);
+                
+                issueData.add("Motorbike information: ");
+                qr = "SELECT * FROM MOTORBIKE WHERE idMotorbike = '" + mtbID + "'";
+                ResultSet rst = dbHandler.excQuery(qr);
+                while(rst.next()){
+                    issueData.add("Producer: " + rst.getString("producer"));
+                    issueData.add("Name: " + rst.getString("name"));
+                    issueData.add("Color: " + rst.getString("color"));
+                }
+                
+                qr = "SELECT * FROM MEMBER WHERE idMember = '" + mmbID + "'";
+                rst = dbHandler.excQuery(qr);
+                issueData.add("Member information: ");
+                while(rst.next()){
+                    issueData.add("Name: " + rst.getString("name"));
+                    issueData.add("Mobile: " + rst.getString("mobile"));
+                    issueData.add("Email: " + rst.getString("email"));
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        lsvIssueData.getItems().setAll(issueData);
     }
     
 }
